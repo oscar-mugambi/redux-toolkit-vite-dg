7a9e6be5fad76b7151fa7ebdad8867565faff17c
oscar mugambi <oscarmugambi@gmail.com>
2022-08-17 20:00:31 +0300

    configured redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import Counter from './features/counter/Counter'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Counter />
+    </div>
+  )
 }
 
 export default App
